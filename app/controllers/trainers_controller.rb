331b34534ced61442c475e34a54da7c6be48078c
Rai <raikcuerda19@gmail.com>
2020-05-21 18:09:40 -0500

    add pokemos for trainers

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,11 +1,20 @@
 class TrainersController < ApplicationController
     def index
-        @trainer = Trainer.all   
-        render json: @trainer 
+        if params[:trainer_id]
+            @trainer = Trainer.find(params[:trainer_id])
+            @pokemon = @trainer.pokemons
+            render json: @pokemon 
+        else
+            @trainer = Trainer.all   
+            render json: @trainer 
+        end
     end
+
+    
 
     def show
         @trainer = Trainer.find(params[:id])
         render json: @trainer
     end
+    
 end
