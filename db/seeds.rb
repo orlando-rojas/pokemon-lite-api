3b0c5ce46a06704470d743882a02fc9d4fa4ff94
Gerard Altamirano <galtamiranor@outlook.es>
2020-05-21 16:03:43 -0500

    Populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,32 @@
 #
 #   movies g= Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
 
+data = CSV.read('db/data.csv', headers: true, header_converters: :symbol)
 
+#"t_name","t_gender","t_region","t_team_member",
+# p csv.headers
+
+data.each do |row|
+  trainer = Trainer.find_or_create_by!(
+      name: row[:t_name],
+      gender: row[:t_gender],
+      home_region: row[:t_region],
+      status: row[:t_team_member],
+      wins: row[:wins],
+      losses: row[:losses]
+  )
+
+  pokemon = Pokemon.find_or_create_by!(
+      name: row[:name],
+      main_ability: row[:main_ability],
+      main_type: row[:main_type],
+      base_experience: row[:base_exp]
+  )
+
+  if trainer.id && pokemon.id
+    trainer.pokemons << pokemon
+  end
+
+end
