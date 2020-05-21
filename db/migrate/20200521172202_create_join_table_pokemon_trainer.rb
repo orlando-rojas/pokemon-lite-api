e7ee3a35944ef33e070c7cd0e562ab586ece442d
Gerard Altamirano <galtamiranor@outlook.es>
2020-05-21 12:32:05 -0500

    Create Pokemon, Trainer models and Join Table

--- db/migrate/20200521172202_create_join_table_pokemon_trainer.rb
+++ db/migrate/20200521172202_create_join_table_pokemon_trainer.rb
@@ -1,8 +1,8 @@
 class CreateJoinTablePokemonTrainer < ActiveRecord::Migration[6.0]
   def change
     create_join_table :Pokemons, :Trainers do |t|
-      t.index [:pokemon_id, :trainer_id]
-      t.index [:trainer_id, :pokemon_id]
+      # t.index [:pokemon_id, :trainer_id]
+      # t.index [:trainer_id, :pokemon_id]
     end
   end
 end
