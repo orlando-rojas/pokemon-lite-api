b71c843bcd30662b22212d67015a6a4d279e0045
Gerard Altamirano <galtamiranor@outlook.es>
2020-05-21 15:47:41 -0500

    Add validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,26 @@
 class Pokemon < ApplicationRecord
   has_and_belongs_to_many :trainers
 
+  validates :name, :main_ability, :main_type, presence: true
+
+  validates :name, uniqueness: true
+
+  validates :main_type, inclusion: { in: [
+      'bug',
+      'electric',
+      'fairy',
+      'fighting',
+      'fire',
+      'flying',
+      'grass',
+      'ground',
+      'ice',
+      'normal',
+      'poison',
+      'psychic',
+      'rock',
+      'steel',
+      'water'
+  ]}
+
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,3 +1,7 @@
 class Trainer < ApplicationRecord
   has_and_belongs_to_many :pokemons
+  validates :name, :gender, presence: true
+  validates :name, uniqueness: true
+  validates :status, inclusion: { in: [true, false] }
+  validates :wins, :losses, numericality: { greater_than_or_equal_to: 0  }
 end
